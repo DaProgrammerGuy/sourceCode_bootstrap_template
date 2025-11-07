fabd98a634ad845720aa8896a4d171158d83b3db
Uzair Ahmed <uzair.ahmed.me@gmail.com>
2025-11-07 12:02:49 +0500

    Implemented Breeze and modified Login, Register pages to template. Also added Logout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
 use App\Http\Controllers\courseController;
-use App\Http\Controllers\homeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [homeController::class, 'index'])->name('home');
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
 
-Route::get('/get-subcategories/{id}', [courseController::class, 'getSubcategories']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::resource('/courses', courseController::class);
 
-Route::resource('/courses', courseController::class);
+    Route::get('/get-subcategories/{id}', [courseController::class, 'getSubcategories']);
+
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
+
+});
+
+require __DIR__.'/auth.php';
